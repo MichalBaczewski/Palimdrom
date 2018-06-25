9722e01833fd797fef219006edb3dedc72255930
MichalBaczewski <michal.baczewski@outlook.com>
2018-06-25 22:43:33 +0200

    Dodano obsługę wyjątku.

--- src/com/michalbaczewski/main/Main.java
+++ src/com/michalbaczewski/main/Main.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
         Palimdrom palimdrom = new Palimdrom();
         ArrayList<String> stringArrayList = new ArrayList<String>();
-        stringArrayList = palimdrom.fileToList("src/com/michalbaczewski/main/words.txt");
-        System.out.println(palimdrom.palimdromListCheck(stringArrayList));
-
+        try {
+            stringArrayList = palimdrom.fileToList("src/com/michalbaczewski/main/words.txt");
+            System.out.println(palimdrom.palimdromListCheck(stringArrayList));
+        } catch (FileNotFoundException e) {
+            System.out.println("Błąd odczytu danych");
+        }
     }
 }
